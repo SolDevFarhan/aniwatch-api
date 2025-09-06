c071b414ebe7c2b1c9ec30926437605bf5ef9bf7
SolDevFarhan <mdfarhantanvirtalha@gmail.com>
2025-09-06 17:29:45 +0600

    init

---
+++ src/config/logger.ts
@@ -0,0 +1,27 @@
+import { env } from "./env.js";
+import { pino, type LoggerOptions } from "pino";
+
+const loggerOptions: LoggerOptions = {
+    redact: env.isProduction ? ["hostname"] : [],
+    level: "info",
+    transport: env.isDev
+        ? {
+              target: "pino-pretty",
+              options: {
+                  colorize: true,
+                  translateTime: "SYS:standard",
+              },
+          }
+        : undefined,
+    formatters: {
+        level(label) {
+            return {
+                level: label.toUpperCase(),
+            };
+        },
+    },
+    // custom timestamp instead of stdTimeFunctions
+    timestamp: () => `,"time":"${new Date().toISOString()}"`,
+};
+
+export const log = pino(loggerOptions);
